f04138d77d05998804f25617ecb120e144f7840c
agouy <contact@agouy.com>
2020-11-13 08:39:41 +0000

    operator handles zip files + checks FCS

--- README.md
+++ README.md
@@ -8,14 +8,15 @@
 
 Input projection|.
 ---|---
-`documentId`        | is the documentId 
+`documentId`        | is the documentId (document can be a single FCS file, or a zipped set of FCS files)
 
 Output relations|.
 ---|---
-`documentId`          | character, the id of the document
+`filename`          | character, the name of the FCS file
+`channels`          | numeric, one variable per channel in the FCS file
 
 ##### Details
 
-Transforms FCS files to Tercen dataset. 
+The operator transforms FCS files into Tercen dataset. If the document is a ZIP file containing a set of FCS files, the operator extracts the FCS files and tranforms them into Tercen datasets.
 
 The Flow Cytometry Standard is a data file standard for the reading and writing of data from flow cytometry experiments.
--- main.R
+++ main.R
@@ -4,41 +4,74 @@ library(dplyr)
 library(flowCore)
 
 # http://localhost:53322/index.html#/alex/w/e11cea9d1119c8e6dbb0842925032c6e/ds/91c7e052-54e6-49b8-bf2c-09f55c5762e3
-# options("tercen.serviceUri"="http://172.17.0.1:5400/api/v1/")
-# options("tercen.workflowId"= "e11cea9d1119c8e6dbb0842925032c6e")
-# options("tercen.stepId"= "91c7e052-54e6-49b8-bf2c-09f55c5762e3")
- 
-doc_to_data = function(df){
-  docId = df$documentId[1]
-  doc = ctx$client$fileService$get(docId)
-  filename = tempfile()
-  writeBin(ctx$client$fileService$download(docId), filename)
-  on.exit(unlink(filename))
+# options("tercen.serviceUri"="http://172.17.0.1:5400/api/v1/") 
+# options("tercen.workflowId"= "132ff39e2530caca083900267b00283f")
+# options("tercen.stepId"= "124f89fc-f3b3-4ebd-9abc-3f26d3f0fe61")
+
+fds_to_data = function(filename) {
+  
   data_fcs = read.FCS(filename, transformation = FALSE)
   names_parameters = data_fcs@parameters@data$desc
   data = as.data.frame(exprs(data_fcs))
   col_names = colnames(data)
   names_parameters = ifelse(is.na(names_parameters),col_names,names_parameters)
   colnames(data) = names_parameters
-  print(class(data))
   data %>%
     mutate_if(is.logical, as.character) %>%
     mutate_if(is.integer, as.double) %>%
-    mutate(.ci= rep_len(df$.ci[1], nrow(.))) %>%
-    mutate(filename= rep_len(doc$name, nrow(.)))
+    mutate(.ci = rep_len(0, nrow(.))) %>%
+    mutate(filename = rep_len(basename(filename), nrow(.)))
 }
 
 ctx = tercenCtx()
 
 if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
 
-ctx$cselect() %>% 
-  mutate(.ci= 1:nrow(.)-1) %>%
-  split(.$.ci) %>%
-  lapply(doc_to_data) %>%
+#1. extract files
+df <- ctx$cselect()
+
+docId = df$documentId[1]
+doc = ctx$client$fileService$get(docId)
+filename = doc$name
+writeBin(ctx$client$fileService$download(docId), filename)
+on.exit(unlink(filename))
+
+# unzip if archive
+if(length(grep(".zip", filename)) > 0) {
+  tmpdir <- tempfile()
+  unzip(filename, exdir = tmpdir)
+  f.names <- list.files(tmpdir, full.names = TRUE)
+} else {
+  f.names <- filename
+}
+
+# check FCS
+if(any(!isFCSfile(f.names))) stop("Not all imported files are FCS files.")
+
+assign("actual", 0, envir = .GlobalEnv)
+task = ctx$task
+
+assign
+
+#2. convert them to FDS files
+df <- f.names %>%
+  lapply(function(filename){
+    data = fds_to_data(filename)
+    if (!is.null(task)) {
+      # task is null when run from RStudio
+      actual = get("actual",  envir = .GlobalEnv) + 1
+      assign("actual", actual, envir = .GlobalEnv)
+      evt = TaskProgressEvent$new()
+      evt$taskId = task$id
+      evt$total = length(f.names)
+      evt$actual = actual
+      evt$message = paste0('processing FDS file ' , filename)
+      ctx$client$eventService$sendChannel(task$channelId, evt)
+    } else {
+      cat('processing FDS file ' , filename)
+    }
+    data
+  }) %>%
   bind_rows() %>%
   ctx$addNamespace() %>%
   ctx$save()
-
-
-
