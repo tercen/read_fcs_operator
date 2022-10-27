0975cb140dd2f0d5e657fbb420a6d4e402d1d750
agouy <contact@agouy.com>
2022-10-27 08:18:03 +0000

    output compensation matrices

--- main.R
+++ main.R
@@ -1,166 +1,87 @@
-library(tercen)
-library(tercenApi)
-library(dplyr, warn.conflicts = FALSE)
-library(flowCore)
+suppressPackageStartupMessages({
+  library(tercen)
+  library(tercenApi)
+  library(dplyr, warn.conflicts = FALSE)
+  library(flowCore)
+})
 
-read_fcs <- function(filename, use.comp, csv.comp, which.lines, separator, use.descriptions) {
-  
-  data_fcs <- read.FCS(
-    filename,
-    transformation = FALSE,
-    which.lines = NULL,
-    dataset = 2,
-    emptyValue = FALSE,
-    ignore.text.offset = TRUE
-  )
-  
-  if(!is.null(which.lines)) {
-    nr <- nrow(data_fcs)
-    idx <- sample(x = nr, size = min(which.lines, nr))
-    data_fcs <- data_fcs[idx, ]
-  }
-
-  # Prepare parameters names
-  na_desc_idx <- is.na(data_fcs@parameters@data$desc)
-  dup_desc_idx <- duplicated(data_fcs@parameters@data$desc)
-  data_fcs@parameters@data$desc[na_desc_idx] <- data_fcs@parameters@data$name[na_desc_idx]
-  data_fcs@parameters@data$desc[dup_desc_idx] <- data_fcs@parameters@data$name[dup_desc_idx]
-  
-  desc_parameters <- data_fcs@parameters@data$desc
-  
-  # Do compensation
-  comp.name <- desc_parameters[!(grepl("[FS]S|Time|TIME|Width|Index|Event|Blank", desc_parameters))]
-  
-  if (use.comp) {
-    
-    spill.matrix <- spillover(data_fcs)[!unlist(lapply(spillover(data_fcs), is.null))]
-    
-    if(length(spill.matrix) > 1) {
-      stop("Multiple compensation matrices found. Compensation cannot be applied.")
-    } else if(length(comp.name) != length(colnames(spill.matrix[[1]]))) {
-      stop("Different number of columns between FCS file and compensation matrix. Compensation cannot be applied.")
-    } else {
-      data_fcs = compensate(data_fcs, spill.matrix[[1]])
-    }
-    
-  } else if(!is.null(csv.comp)) {
-    
-    if (separator == "Comma") {
-      matrix.com <- read.csv(csv.comp, header = TRUE, sep = ",", row.names = 1)  
-    } else if (separator == "Tab"){
-      matrix.com <- read.table(csv.comp, header = TRUE, row.names = 1)  
-    } else if  (separator == "Semicolon"){
-      matrix.com <- read.csv(csv.comp, header = TRUE, sep = ";", row.names = 1) 
-    }
-    
-    if (length(comp.name) == length(colnames(matrix.com))) {
-      colnames(matrix.com) <- comp.name
-      data_fcs <- compensate(data_fcs, matrix.com)
-    } else {
-      stop("Different number of columns between FCS file and compensation matrix. Compensation cannot be applied.")
-    }
-    
-  }
-  
-  data <- as.data.frame(exprs(data_fcs))
-  col_names <- colnames(data)
-  if(use.descriptions) {
-    desc_parameters <- ifelse(is.na(desc_parameters), col_names, desc_parameters)
-    colnames(data) <- desc_parameters
-  }
-  
-  return.data <- data %>%
-    mutate_if(is.logical, as.character) %>%
-    mutate_if(is.integer, as.double) %>%
-    mutate(.ci = as.integer(rep_len(0, nrow(.)))) %>%
-    # mutate(dirname = rep_len(dirname(filename), nrow(.))) %>%
-    mutate(filename = rep_len(basename(filename), nrow(.)))
-  
-  return(return.data)
-}
+source("./utils.R")
 
 ctx <- tercenCtx()
 
-if (!any(ctx$cnames == "documentId"))
+if (!any(ctx$cnames == "documentId")) {
   stop("Column factor documentId is required")
+}
 
-comp.use <- ctx$op.value("use.builtin.compensation", as.logical, FALSE)
-custom.comp.use <- ctx$op.value("use.custom.compensation", as.logical, FALSE)
-
-use.descriptions <- ctx$op.value("use.descriptions", as.character, TRUE)
-
-# if both options are set to TRUE, custom compensation is used
-if (comp.use & custom.comp.use) comp.use <- FALSE
-
+use.descriptions <- ctx$op.value("use.descriptions", as.logical, FALSE)
+output.compensation <- ctx$op.value("output.compensation", as.logical, TRUE)
 separator <- ctx$op.value("Separator", as.character, "Comma")
 which.lines <- ctx$op.value("which.lines", as.double, -1)
 if(which.lines == -1) which.lines <- NULL
 
 # 1. Extract files
-df <- ctx$cselect()
-docId <- df$documentId[1]
-doc <- ctx$client$fileService$get(docId)
-filename_tmp <- tempfile()
-writeBin(ctx$client$fileService$download(docId), filename_tmp)
-on.exit(unlink(filename_tmp))
-
-# unzip if archive
-if (length(grep(".zip", doc$name)) > 0) {
-  tmpdir <- tempdir()
-  unzip(filename_tmp, exdir = tmpdir)
-  f.names <- list.files(
-    tmpdir,
-    full.names = TRUE,
-    pattern = "(\\.fcs$|\\.lmd$|\\.LMD$)",
-    ignore.case = TRUE,
-    recursive = TRUE
-  )
-  if (custom.comp.use){
-    c.names <- list.files(
-      tmpdir,
-      full.names = TRUE,
-      pattern = "(\\comp.txt$|\\comp.csv$|\\comp.tsv$)",
-      ignore.case = TRUE,
-      recursive = TRUE
-    )
-    c.names <- c.names[!grepl("renv", c.names)]
-    
-    if (length(c.names) != 1) {
-      stop("None or more than one CSV compensation matrix found.")
-    }
-    
-  } else {
-    c.names <- NULL
-  }
-} else {
-  f.names <- filename_tmp
-  c.names <- NULL
-}
+files <- download_files(ctx)
+files_prep <- prepare_files(files)
+on.exit(unlink(files_prep$f.names))
 
 assign("actual", 0, envir = .GlobalEnv)
 task = ctx$task
 
 # 2. Read and convert FCS files
-f.names %>%
-  lapply(function(filename) {
+df <- files_prep %>%
+  apply(MARGIN = 1, function(fn) {
     
-    data = read_fcs(filename, comp.use, c.names, which.lines, separator, use.descriptions)
-    
+    out <- list()
+    data <- get_fcs(fn["f.names"], which.lines)
+    if(output.compensation) {
+      out$spill.matrix <- get_spill_matrix(data, csv.comp = fn["c.names"], separator = separator) %>% 
+        as_tibble() %>%
+        ctx$addNamespace() %>%
+        as.matrix()
+    } else {
+      out$spill.matrix <- NA
+    }
+    out$data <- process_fcs(data, use.descriptions)
+
     if (!is.null(task)) {
       # task is null when run from RStudio
       actual = get("actual",  envir = .GlobalEnv) + 1
       assign("actual", actual, envir = .GlobalEnv)
       evt = TaskProgressEvent$new()
       evt$taskId = task$id
-      evt$total = length(f.names)
+      evt$total = length(files_prep$f.names)
       evt$actual = actual
-      evt$message = paste0('Processing FCS file: ' , filename)
+      evt$message = paste0('Processing FCS file: ' , fn["f.names"])
       ctx$client$eventService$sendChannel(task$channelId, evt)
     } else {
-      cat('Processing FCS file: ' , filename)
+      cat('Processing FCS file: ' , fn["f.names"])
     }
-    data
-  }) %>%
-  bind_rows() %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+    out
+  })
+
+df_out <- lapply(df, "[[", "data") %>%
+  bind_rows()
+
+if(output.compensation) {
+  spill.list <- lapply(df, "[[", "spill.matrix")
+  names(spill.list) <- basename(files_prep$f.names)
+  
+  df_comp <- data.frame(
+    compensation_matrices = "compensation_matrices",
+    .base64.serialized.r.model = c(serialize_to_string(spill.list))
+  ) %>% 
+    ctx$addNamespace() %>%
+    as_relation()
+  
+  df_out %>%
+    ctx$addNamespace() %>%
+    as_relation() %>%
+    left_join_relation(ctx$crelation, ".ci", ctx$crelation$rids) %>%
+    left_join_relation(df_comp, list(), list()) %>%
+    as_join_operator(ctx$cnames, ctx$cnames) %>%
+    save_relation(ctx)
+
+} else {
+  df_merged <- df_merged %>% ctx$addNamespace()
+  ctx$save(df_merged)
+}
---
+++ utils.R
@@ -0,0 +1,161 @@
+download_files <- function(ctx) {
+  df <- ctx$cselect()
+  if(!"documentId" %in% colnames(df)) stop("documentId factor needs to be projected onto columns.")
+  docId <- df$documentId[1]
+  doc <- ctx$client$fileService$get(docId)
+  filename_tmp <- tempfile(fileext = paste0(".", tools::file_ext(doc$name)))
+  writeBin(ctx$client$fileService$download(docId), filename_tmp)
+  return(list(filename_tmp = filename_tmp, docname = doc$name))
+}
+
+prepare_files <- function(files) {
+  if (length(grep(".zip", files$docname)) > 0) {
+    tmpdir <- tempdir()
+    unzip(files$filename_tmp, exdir = tmpdir)
+    f.names <- list.files(
+      tmpdir,
+      full.names = TRUE,
+      pattern = "(\\.fcs$|\\.lmd$|\\.LMD$)",
+      ignore.case = TRUE,
+      recursive = TRUE
+    )
+    c.names <- list.files(
+      tmpdir,
+      full.names = TRUE,
+      pattern = "(\\comp.txt$|\\comp.csv$|\\comp.tsv$)",
+      ignore.case = TRUE,
+      recursive = TRUE
+    )
+    c.names <- c.names[!grepl("renv", c.names)]
+    
+    if (length(c.names) > 1) {
+      stop("More than one custom compensation matrix found.")
+    } else if (length(c.names) == 0) {
+      c.names <- NA
+    }
+  } else {
+    f.names <- files$filename_tmp
+    c.names <- NA
+  }
+  return(data.frame(f.names = f.names, c.names = c.names))
+}
+
+get_fcs <- function(filename, which.lines) {
+  
+  data_fcs <- read.FCS(
+    filename,
+    transformation = FALSE,
+    which.lines = NULL,
+    dataset = 2,
+    emptyValue = FALSE,
+    ignore.text.offset = TRUE
+  )
+  
+  if(!is.null(which.lines)) {
+    nr <- nrow(data_fcs)
+    idx <- sample(x = nr, size = min(which.lines, nr))
+    data_fcs <- data_fcs[idx, ]
+  }
+  
+  return(data_fcs)
+}
+
+get_spill_matrix <- function(data_fcs, csv.comp, separator) {
+  
+  if(is.na(csv.comp)) {
+    spill.matrix <- spillover(data_fcs)[!unlist(lapply(spillover(data_fcs), is.null))]
+    
+    if(length(spill.matrix) > 1) {
+      stop("Multiple compensation matrices found. Compensation cannot be applied.")
+    } else {
+      spill.matrix <- spill.matrix[[1]]
+    }
+  } else {
+    if (separator == "Comma") {
+      spill.matrix <- read.csv(csv.comp, header = TRUE, sep = ",", row.names = TRUE)  
+    } else if (separator == "Tab"){
+      spill.matrix <- read.table(csv.comp, header = TRUE, row.names = TRUE)
+    } else if  (separator == "Semicolon"){
+      spill.matrix <- read.csv(csv.comp, header = TRUE, sep = ";", row.names = TRUE) 
+    }
+  }
+  
+  return(spill.matrix)
+}
+
+process_fcs <- function(data_fcs, use.descriptions) {
+  
+  # Prepare parameters names
+  na_desc_idx <- is.na(data_fcs@parameters@data$desc)
+  dup_desc_idx <- duplicated(data_fcs@parameters@data$desc)
+  data_fcs@parameters@data$desc[na_desc_idx] <- data_fcs@parameters@data$name[na_desc_idx]
+  data_fcs@parameters@data$desc[dup_desc_idx] <- data_fcs@parameters@data$name[dup_desc_idx]
+  
+  desc_parameters <- data_fcs@parameters@data$desc
+  
+  data <- as.data.frame(exprs(data_fcs))
+  col_names <- colnames(data)
+  if(use.descriptions) {
+    desc_parameters <- ifelse(is.na(desc_parameters), col_names, desc_parameters)
+    colnames(data) <- desc_parameters
+  }
+  
+  fcs.data <- data %>%
+    mutate_if(is.logical, as.character) %>%
+    mutate_if(is.integer, as.double) %>%
+    mutate(.ci = as.integer(rep_len(0, nrow(.)))) %>%
+    mutate(filename = rep_len(basename(data_fcs@description$FILENAME), nrow(.)))
+  
+  return(fcs.data)
+}
+
+serialize_comp <- function (df, object, object_name, ctx) 
+{
+  df$.object <- object_name
+  columnTable <- ctx$cselect() %>% mutate(.ci = 0:(nrow(.) - 
+                                                     1))
+  leftTable <- data.frame(df) %>% ctx$addNamespace() %>% left_join(columnTable, 
+                                                                   by = ".ci") %>% select(-.ci) %>% tercen::dataframe.as.table()
+  leftTable$properties$name = "left"
+  leftRelation <- SimpleRelation$new()
+  leftRelation$id <- leftTable$properties$name
+  rightTable <- data.frame(compensation_matrices = object_name, .base64.serialized.r.model = c(serialize_to_string(object))) %>% 
+    ctx$addNamespace() %>% tercen::dataframe.as.table()
+  rightTable$properties$name <- "right"
+  rightRelation <- SimpleRelation$new()
+  rightRelation$id <- rightTable$properties$name
+  pair <- ColumnPair$new()
+  pair$lColumns <- list(".object")
+  pair$rColumns = list(rightTable$columns[[1]]$name)
+  join.model = JoinOperator$new()
+  join.model$rightRelation = rightRelation
+  join.model$leftPair = pair
+  compositeRelation = CompositeRelation$new()
+  compositeRelation$id = "compositeRelation"
+  compositeRelation$mainRelation = leftRelation
+  compositeRelation$joinOperators = list(join.model)
+  pair_2 <- ColumnPair$new()
+  pair_2$lColumns <- unname(ctx$cnames)
+  pair_2$rColumns = unname(ctx$cnames)
+  join = JoinOperator$new()
+  join$rightRelation = compositeRelation
+  join$leftPair = pair_2
+  result = OperatorResult$new()
+  result$tables = list(leftTable, rightTable)
+  result$joinOperators = list(join)
+  return(result)
+}
+
+serialize_to_string <- function (object) 
+{
+  if (!inherits(object, "list")) 
+    object <- list(object)
+  str64_list <- sapply(object, function(x) {
+    con <- rawConnection(raw(0), "r+")
+    saveRDS(x, con)
+    str64 <- base64enc::base64encode(rawConnectionValue(con))
+    close(con)
+    str64
+  })
+  return((str64_list))
+}
